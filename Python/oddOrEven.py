a35f382cc5265ab23208e3a64aa07b2a69f115b5
Don Heshanthaka <heshanthaka007@gmail.com>
2022-09-28 09:08:14 +0530

    Revert "Add oddOrEven.py into python projects"
    This reverts commit 0be7b2cfda96d85c5afddd4ddc603254131c7f75.

--- Python/oddOrEven.py
+++
@@ -1,9 +0,0 @@
-def odd_or_even(number):
-    if (number % 2 == 0):
-        return 'even'
-    else:
-        return 'odd'
-
-result = odd_or_even(int(input('Enter a number: ')))
-
-print(f'The number you entered is {result}.')
