ad2c38ad063fa142e155b932d1c9706731eeb597
Preetam Shri <110012624+Preetam-Shri@users.noreply.github.com>
2022-10-01 00:20:43 +0530

    madlibs.py
    The main objective of this project is to build a mad lib game by just using core python knowledge. Here in this project, we will allow users to inputs specific words like a noun, adverb, verb, food, animal, adjective, etc., based on the given requirement. And using all the user’s inputs, a story will be generated using python.

---
+++ Python/madlibs.py
@@ -0,0 +1,19 @@
+verb_1 = input("Enter a verb of choice, and press enter:")
+adj_1 = input("Enter a ajective of choice, and press enter:")
+verb_2 = input("Enter second verb of choice, and press enter:")
+body_part = input("Enter a body part name of choice, and press enter:")
+adverb = input("Enter an adverb of choice, and press enter:")
+body_part_2 = input("Enter any body name of your choice,and press enter:")
+noun = input("Enter a noun of choice, and press enter:")
+verb_3 = input("Enter the third verb of choice, and press enter:")
+animal = input("Enter name of any animal of choice, and press enter:")
+noun_2 = input("Enter an noun of choice , and press enter:")
+verb_4 = input("Enter the fourth verb of choice, and press enter:")
+adj_2 = input("Enter an adjective of chioce, and press enter:")
+color = input("Enter any color name, and press enter:")
+
+
+
+story = "Most doctors agree that bicycle of" + verb_1 + " is a/an " + adj_1 + " form of exercise." + verb_2 +" a bicycle enables you to develop your " + body_part + " muscles as well as " + adverb + " increase the rate of a " + body_part_2+" beat. More " + noun + " around the world "+ verb_3 +" bicycles than drive "+ animal +". No matter what kind of "+ noun_2 +"you "+verb_4+", always be sure to wear a/an "+adj_2+" helmet.Make sure to have  " + color + " reflectors too! "
+
+print(story)
