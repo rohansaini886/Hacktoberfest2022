3ae04199685d2592fa3f8dc47676d212dfd9921d
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2022-09-30 21:42:17 +0530

    Create 199-A.cpp

---
+++ codeforces/199-A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n, a = 0, b = 1, x = 1, temp;
+    cin >> n;
+    if (n == 0)
+    {
+        cout << "0 0 0";
+    }
+    else
+    {
+        while (x != n)
+        {
+            a = b;
+            temp = b;
+            b = x;
+            x = x + temp;
+        }
+        cout<<"0 "<<a<<" "<<b;
+    }
+    return 0;
+}
