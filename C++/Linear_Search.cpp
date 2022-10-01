b6d1f0da30aae58576d1989fa5624b2943ae5efb
Lokesh Choudhary Medharametla <73935267+Lokesh-Choudhary@users.noreply.github.com>
2022-10-01 16:52:16 +0530

    Create Linear_Search.cpp

---
+++ C++/Linear_Search.cpp
@@ -0,0 +1,37 @@
+// C++ Recursive Code For Linear Search
+#include <iostream>
+using namespace std;
+ 
+int linearsearch(int arr[], int size, int key)
+{
+    if (size == 0) {
+        return -1;
+    }
+    else if (arr[size - 1] == key) {
+        // Return the index of found key.
+        return size - 1;
+    }
+    else {
+        int ans = linearsearch(arr, size - 1, key);
+        return ans;
+    }
+}
+ 
+// Driver's Code
+int main()
+{
+    int arr[5] = {5, 15, 6, 9, 4 };
+    int key = 4;
+   
+      // Function call
+    int ans = linearsearch(arr, 5, key);
+    if (ans == -1) {
+        cout << "The element " << key << " is not found."
+             << endl;
+    }
+    else {
+        cout << "The element " << key << " is found at "
+             << ans << " index of the given array." << endl;
+    }
+    return 0;
+}
