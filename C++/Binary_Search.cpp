bf1d7d2e6ed6e414d940124aeee2f7bcdd578766
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2022-10-01 15:20:36 +0530

    Update Binary_Search.cpp

--- C++/Binary_Search.cpp
+++ C++/Binary_Search.cpp
@@ -1,16 +1,31 @@
-//Binary Search
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+int binary_search(int A[], int left, int right, int key) 
+{
+  int m;
+  while( left <= right )     
+  {
+    m = left + (right-left)/2;
+    if( A[m] == key )     // Element found
+      return m;
+    if( A[m] < key )         // Search in right part of list
+      left = m + 1;
+    else            // Search in left part of list
+      right = m - 1;
+  }
+  return -1;
+}
 
-//only logic part
-
-class Solution {
-  public:
-    int binarysearch(int arr[], int n, int k) {
-        // code here
-        for(int i=0;i<n;i++){
-            if(arr[i] == k){
-                return i;
-            }
-        }
-        return -1;
-    }
-};
+int main() 
+{
+  int loc, x, array[]={10,11,12,13,14,25,26,37,48,59};
+  x = 26;        // element to be searched in the array
+  loc=binary_search(array,0,10,x);
+  if(loc != -1)
+    cout<<"Element found at location : "<<loc;
+  else
+    cout<<"Element not present in the array.";
+  return 0;
+}
