da45792db503fb3d54009116565cfe8e17d09ee7
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2022-10-01 14:50:55 +0530

    Create 1537B.cpp

---
+++ codeforces/1537B.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(){
+	int n,m,i,j;
+    cin>>n>>m>>i>>j;
+    cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+}
+ 
+int main(){
+   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+   
+   int t; cin>>t; while(t--)
+ 
+   solve();
+ 
+   return 0;
+}
